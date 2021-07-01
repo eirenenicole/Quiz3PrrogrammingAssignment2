8a8a87d5e4156d47cd86fa440515c8928c645370
eirenenicole <86765334+eirenenicole@users.noreply.github.com>
2021-07-01 14:43:55 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-setwd('C:/Users/rubind1/Documents/Coursera-R')
-##
+
 makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
   s <- NULL
   set <- function(y) {
@@ -13,8 +12,8 @@ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
        setsolve = setsolve,
        getsolve = getsolve)
 }
-##
-## Same here, changed "mean" to "solve" and "m" to "s"
+
+
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
   if(!is.null(s)) {
